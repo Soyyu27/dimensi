3bcc8afd0c3407ab03dc7c1ece4fc86b448ecc82
heisoyyy <heikelqushoyi@gmail.com>
2025-09-25 13:17:59 +0700

    yesss

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Dimensi/',  // wajib sesuai nama repo
+  base: '/', // HARUS sama dengan nama repo
   plugins: [react()]
 })
